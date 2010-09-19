b052d47536e44430dea0c3667dc3463a8d42a7e6
Ivan Vanderbyl <ivanvanderbyl@me.com>
2010-09-19 15:52:35 +1000

    First commit

---
+++ javascripts/progress.js
@@ -0,0 +1,77 @@
+// Copyright (c) 2010 Ivan Vanderbyl
+// Originally found at http://ivan.ly/ui
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+(function( $ ){
+  // Simple wrapper around jQuery animate to simplify animating progress from your app
+  // Inputs: Progress as a percent, Callback
+  $.fn.animateProgress = function(progress, callback) {    
+    return this.each(function() {
+      $(this).animate({
+        width: progress+'%'
+      }, {
+        duration: 2000, 
+        
+        // swing or linear
+        easing: 'swing',
+
+        // this gets called every step of the animation, and updates the label
+        step: function( progress ){
+          var labelEl = $('.ui-label', this),
+              valueEl = $('.value', labelEl);
+          
+          if (Math.ceil(progress) < 20 && $('.ui-label', this).is(":visible")) {
+            labelEl.hide();
+          }else{
+            if (labelEl.is(":hidden")) {
+              labelEl.fadeIn();
+            };
+          }
+          
+          if (Math.ceil(progress) == 100) {
+            labelEl.text('Done');
+            setTimeout(function() {
+              labelEl.fadeOut();
+            }, 1000);
+          }else{
+            valueEl.text(Math.ceil(progress) + '%');
+          }
+        },
+        complete: function(scope, i, elem) {
+          if (callback) {
+            callback.call(this, i, elem );
+          };
+        }
+      });
+    });
+  };
+})( jQuery );
+
+// Hide the label at start
+$('#progress_bar .ui-progress .ui-label').hide();
+
+// Simulate some progress
+$('#progress_bar .ui-progress').animateProgress(43, function() {
+  $(this).animateProgress(79, function() {
+    setTimeout(function() {
+      $('#progress_bar .ui-progress').animateProgress(100);
+    }, 2000);
+  });
+});
